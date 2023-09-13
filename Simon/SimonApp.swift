30d3926a14f9b2bbbd15c3166d9b5d4b2da71a62
Josh Madison <jmad99@sbcglobal.net>
2023-09-13 09:34:04 -0500

    Initial Commit

---
+++ Simon/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Simon
+//
+//  Created by Josh Madison on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
