da98d74056c96ae1b931137bd8870d1b8fd5d515
Josh Madison <jmad99@sbcglobal.net>
2023-09-21 09:23:12 -0500

    Created Simon View

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colorDisplay = [ColorDisplay(color: .green), ColorDisplay(color: .red), ColorDisplay(color: .yellow), ColorDisplay(color: .blue)]
+    @State private var flash = [false, false, false, false]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Simon")
+                
+            HStack {
+                colorDisplay[0]
+                colorDisplay[1]
+            }
+            HStack {
+                colorDisplay[2]
+                colorDisplay[3]
+            }
         }
         .padding()
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ColorDisplay: View {
+    let color: Color
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(color)
+            .frame(width: 100, height: 100, alignment: .center)
+            .padding()
     }
 }
 
