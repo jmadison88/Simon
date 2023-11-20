276c2f65da54566022a4b3eea7b892aa9b92928c
Josh Madison <jmad99@sbcglobal.net>
2023-11-20 13:05:27 -0600

    Fixed all variables

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -6,13 +6,23 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
-    @State private var colorDisplay = [ColorDisplay(color: .green), ColorDisplay(color: .red), ColorDisplay(color: .yellow), ColorDisplay(color: .blue)]
     @State private var flash = [false, false, false, false]
+    @State private var index = 0
+    @State private var playerTurn = false
+    @State private var messsage = ""
+    @State private var gameOver = true
+    @State var speed = 0.5
+    @State var highScore = 0
+    @State private var timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    @State var audioPlayer: AVAudioPlayer!
+    var colorDisplay = [TapColor(color: .green), [TapColor(color: .red), [TapColor(color: .yellow), [TapColor(color: .blue)  ]
     var body: some View {
         VStack {
             Text("Simon")
+                .font(.custom("Impact", size: 72))
             HStack {
                 colorDisplay[0]
                     .onTapGesture {
@@ -40,6 +50,15 @@ struct ContentView: View {
         }
         .padding()
         .preferredColorScheme(.dark)
+        .onReceive(timer) { _ in
+            if index < sequence.count {
+                flashColorDisplay(index: sequence[index])
+                index += 1
+            }
+            else {
+               index = 0
+            }
+        }
     }
     
     func flashColorDisplay(index: Int) {
